a49fbaa8e3c269d2cda8cc534b5e7c1fb5096c78
Paul Miller <paul@paulmillr.com>
2016-03-13 19:11:52 +0200

    Dump es6. Make this work in all environments.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-module.exports = (callback) => function() {
-  'use strict';
+module.exports = function(callback) { return function() {
+  'use strict'; // MIT License, paulmillr.com
   if (typeof callback !== 'function') throw new TypeError('promisify must receive a function');
   for (var args = new Array(arguments.length), i = 0; i < args.length; ++i) args[i] = arguments[i]; // git.io/vk55A
-  return new Promise((resolve, reject) => {
-    args.push((error, data) => error == null ? resolve(data) : reject(error));
-    callback.apply(this, args);
+  var _this = this;
+  return new Promise(function(resolve, reject) {
+    args.push(function(error, data) {error == null ? resolve(data) : reject(error)});
+    callback.apply(_this, args);
   });
-};
+}};
