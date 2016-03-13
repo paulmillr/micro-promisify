2f43f025a586a22ac3f3ff51d66aa6782e88c41b
Paul Miller <paul@paulmillr.com>
2016-03-13 19:08:56 +0200

    Optimize a bit. Closes gh-1.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 module.exports = (callback) => function() {
+  'use strict';
   if (typeof callback !== 'function') throw new TypeError('promisify must receive a function');
-  const args = [].slice.call(arguments);
+  for (var args = new Array(arguments.length), i = 0; i < args.length; ++i) args[i] = arguments[i]; // git.io/vk55A
   return new Promise((resolve, reject) => {
-    args.push((error, data) => {
-      if (error == null) resolve(data); else reject(error);
-    });
-    callback.apply(null, args);
+    args.push((error, data) => error == null ? resolve(data) : reject(error));
+    callback.apply(this, args);
   });
 };
