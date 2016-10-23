d5a64ef49031e61944b7c8c8de32a60e28108d46
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-10-23 04:43:01 +0300

    Define `length` and `name` properties
    Fixes #3

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-module.exports = function(callback) { return function() {
+module.exports = fn => {
   'use strict'; // MIT License, paulmillr.com
-  if (typeof callback !== 'function') throw new TypeError('promisify must receive a function');
-  for (var args = new Array(arguments.length), i = 0; i < args.length; ++i) args[i] = arguments[i]; // git.io/vk55A
-  var _this = this;
-  return new Promise(function(resolve, reject) {
-    args.push(function(error, data) {error == null ? resolve(data) : reject(error)});
-    callback.apply(_this, args);
+  if (typeof fn !== 'function') throw new TypeError('micro-promisify must receive a function');
+  return Object.defineProperties(function() {
+    for (var args = new Array(arguments.length), i = 0; i < args.length; ++i) args[i] = arguments[i]; // git.io/vk55A
+    return new Promise((resolve, reject) => {
+      args.push((error, result) => error == null ? resolve(result) : reject(error));
+      fn.apply(this, args);
+    });
+  }, {
+    length: {value: Math.max(0, fn.length - 1)},
+    name: {value: fn.name},
   });
-}};
+};
